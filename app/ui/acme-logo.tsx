13e01aa88359a5078c54f30676f2f277ae853a77
ramaditya29 <ramaditya29@gmail.com>
2024-02-16 16:53:46 -0800

    First commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -2,6 +2,7 @@ import { GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
+  
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
